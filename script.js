f5ea109ec137d0a4be09c267f95d21cf1b37812e
Urfan <7nquqmr@code.edu.az>
2022-10-28 00:32:36 +0300

    js

---
+++ script.js
@@ -0,0 +1,43 @@
+//Task1.js 
+let number1=12,number2=14;
+console.log("Task1.the product of two numbers=",number1*number2)
+//Task2.js
+let number3=2,number4=3;
+console.log("Task2.strength=",number3**number4)
+//Task3.js
+let number=123;
+number=number/10;
+console.log("Task3.result=",number)
+//Task4.js
+let number5=125;
+console.log("Task4.Result=",number5%10)
+//Task5.js
+let n1=2345,son;
+let n2=parseInt (n1/ 1000);
+let n3=parseInt(n1/100)%10;
+let n4=parseInt(n1/10)%10;
+let n5=n1%10;
+son=n5*1000+n4*100+n3*10+n2;
+console.log("Task5.Result",son)
+
+//Task6.js
+ let balans=35,price=12,residualmoney,productnum;
+
+        residualmoney=balans%price;
+        productnum=(balans-residualmoney)/price;
+
+        console.log("Task6.Productnum= ",productnum);
+
+    //Task7.js
+     let sidecub=5,valume,surfacarera;
+        valume=sidecub*sidecub*sidecub;
+        surfacarera=6*sidecub*sidecub;
+        console.log("Task7.Volume= ",valume);
+        console.log("Task7.Surfacarera= ",surfacarera);
+        //Task8.js
+        let number8=125,number9,x;
+        let number10=parseInt(number8/10);
+        number9=number8%10;
+        x=number9*100+number10;
+        console.log("Takt8.X in qiymeti",x)
+
